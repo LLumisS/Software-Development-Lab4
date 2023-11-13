e19a025db15dca021820ae25db9b1d39b567e4f1
LLumisS <vasa16671@gmail.com>
2023-11-13 22:32:25 +0200

    lab4

---
+++ src/main/java/org/example/Cloth.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Cloth {
+    private static int currentId = 0;
+
+    private int id;
+    private String type;
+    private String size;
+    private String color;
+    private double price;
+
+    Cloth (String type, String size, String color, double price) {
+        this.id = currentId++;
+        this.type = type;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Cloth> clothes = new ArrayList<>();
+
+        clothes.add(new Cloth("jeans", "L", "blue", 2.99));
+        clothes.add(new Cloth("t-shirt", "M", "red", 1.99));
+        clothes.add(new Cloth("jacket", "XL", "black", 5.99));
+        clothes.add(new Cloth("pants", "S", "gray", 3.49));
+        clothes.add(new Cloth("shorts", "XXL", "pink", 9.99));
+
+        Collections.sort(clothes, Comparator.comparingDouble(Cloth::getPrice).reversed());
+        
+        System.out.println();
+        showList(clothes);
+        
+        Collections.sort(clothes, Comparator.comparingInt(Cloth::getId));
+
+        showList(clothes);
+    }
+
+    private static void showList(ArrayList<Cloth> clothes) {
+        for (Cloth cloth : clothes) {
+            System.out.println(cloth.getId() + "\t" + cloth.getType() + "\t" + 
+                cloth.getSize() + "\t" + cloth.getColor() + "\t" + cloth.getPrice());
+        }
+        System.out.println();
     }
 }
